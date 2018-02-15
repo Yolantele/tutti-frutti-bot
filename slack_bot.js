b0d5431e7dd61180146c98dac6f3d2f2447aa6a8
Jolanta Jasiulionyte <jolanta@Jolantas-MacBook-Pro.local>
2018-02-15 17:37:36 +0000

    Stores total Order and Runs through basic chast flow

--- slack_bot.js
+++ slack_bot.js
@@ -14,9 +14,9 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
-const order = ['fruit order', 'order fruit', '(.*) fruit order', '(.*) order fruit', 'order fruit(.*)', 'fruit order(.*)']
+var totalOrder = [];
 
-controller.hears(order, 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears('I want to order fruits', 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
         timestamp: message.ts,
@@ -31,14 +31,15 @@ controller.hears(order, 'direct_message,direct_mention,mention', function(bot, m
 
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message, 'Ok ' + user.name + ' start your order!!');
+            bot.reply(message, 'Ok ' + user.name + ' let us start your order!!');
         } else {
             bot.reply(message, 'Hello let us start your fruit order.');
         }
+        bot.reply(message, 'Fruits of today are apples and pears, please enter fruit name and quantity')
     });
 });
 
-const fruit = ['(.*) apples (.*)']
+const fruit = ['apple','apples', 'pear', 'pears'];
 
 controller.hears(fruit, 'direct_message,direct_mention,mention', function(bot, message) {
 
@@ -52,7 +53,7 @@ controller.hears(fruit, 'direct_message,direct_mention,mention', function(bot, m
         }
     });
 
-    var choice = message.match[0];
+    var choice = message.text;
     controller.storage.users.get(message.user, function(err, user) {
         if (!user) {
             user = {
@@ -60,34 +61,14 @@ controller.hears(fruit, 'direct_message,direct_mention,mention', function(bot, m
             };
         }
         user.choice = choice;
+        totalOrder.push(choice);
         controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will order you ' + user.choice);
+            bot.reply(message, 'Got it. I will order you ' + message.text);
         });
     });
 
 });
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
 
 controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     var name = message.match[1];
@@ -104,84 +85,17 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_men
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
 
 
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['order done'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.startConversation(message, function(err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?', [
+        convo.ask('Are you sure order is complete', [
             {
                 pattern: bot.utterances.yes,
                 callback: function(response, convo) {
-                    convo.say('Bye!');
+                    convo.say('Order Sent, I will order ' + totalOrder.toString());
                     convo.next();
                     setTimeout(function() {
                         process.exit();
@@ -192,41 +106,12 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
             pattern: bot.utterances.no,
             default: true,
             callback: function(response, convo) {
-                convo.say('*Phew!*');
+                convo.say('*What else woould you like to order*');
                 convo.next();
             }
+            
         }
-        ]);
-    });
+    ]);
+    // totalOrder = [];
 });
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
+});
