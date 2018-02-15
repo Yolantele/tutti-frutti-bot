cdd2319f36e837d3b7ded46def971b1cccd5d31f
Jolanta Jasiulionyte <jolanta@Jolantas-MacBook-Pro.local>
2018-02-15 12:06:34 +0000

    Sends notification with incoming ebhooks

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,12 @@
 const { IncomingWebhook, WebClient } = require('@slack/client');
 
 console.log('Getting started with Slack Developer Kit for Node.js');
+
+const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+const currentTime = new Date().toTimeString();
+timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
+    if (error) {
+        return console.error(error);
+    }
+    console.log('Notification sent');
+});
